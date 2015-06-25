573dc1d55dd8b9b8950949b4dcd904d6d7864ee0
Jonathan Peypops <jonathanpeypops@msn.com>
2015-06-25 13:27:53 +0200

    Added Beans : DataSource, EMF, JpaTransactionManager + pom update

--- src/main/java/be/vdab/films/ApplicationConfiguration.java
+++ src/main/java/be/vdab/films/ApplicationConfiguration.java
@@ -1,9 +1,46 @@
 package be.vdab.films;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("be.vdab")
 public class ApplicationConfiguration {
+
+    @Bean
+    public DataSource dataSource () {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/films");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory (DataSource dataSource){
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setPackagesToScan("be.vdab");
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setShowSql(true);
+        entityManagerFactoryBean.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager jpaTransactionManager (EntityManagerFactory entityManagerFactory){
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 }
