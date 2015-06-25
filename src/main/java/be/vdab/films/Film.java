cb162b916163f1dec7d45a1e5569ae13219ab1e0
Jonathan Peypops <jonathanpeypops@msn.com>
2015-06-25 13:37:02 +0200

    Film code

---
+++ src/main/java/be/vdab/films/Film.java
@@ -0,0 +1,37 @@
+package be.vdab.films;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    protected Film() {
+
+    }
+
+    public Film(String title) {this.title = title;}
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
